077a493ed317eea3954d2e72ca55871de08422cf
unknown <kqyz12@gmail.com>
2024-11-12 15:21:32 +0300

    pseodocode done

---
+++ script.js
@@ -0,0 +1,20 @@
+//variables
+// 1- I will create a function named "startGame" to start the game and restart the game.
+
+// 2- I will create a function named "GiveUp" to end the game.
+
+// 3- I will create a function named "CheckWin" to check if the user win by checking if all the letter was correct and in the right position it will consider "playerWin" else "tryAgain"
+
+// 4- I will create a function named "playerWin" if the user win the game by putting all the letter correct and in the right position.
+
+// 5- I will create a function named "tryAgain" if the user lose the game by putting the wrong letter.
+
+// I wil create function named "retry" is the user cannot guess the right letters.
+
+// 4- I will create a function named "checkPosition" to check if its in the right position, if the letter right it will be green, if not right position and its correct letter it will be yellow
+
+//I will create an if statement if the user press any letter it will go inside the box
+
+// I will do an if statement to check if the letter was correct and in the right position it will flip the box and make it green background, and I will create another if condition if the letter correct but the position not correct it will flip the box and make it yellow background, also if the letter was wrong letter it will keep it grey background.
+
+// I will addEventListener to check guess feedback on key presses As each letter is typed, check if it’s correct (green), partially correct (yellow), or incorrect (grey).
